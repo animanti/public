8746f1c13fd808a93854528a1815e35834f477ae
animanti <github@mccaull.com.au>
2021-05-10 23:47:16 +1000

    1

--- scripts/deploy_emby.sh
+++ scripts/deploy_emby.sh
@@ -1,4 +1,5 @@
 sudo apt update && sudo apt upgrade
 sudo wget https://github.com/MediaBrowser/Emby.Releases/releases/download/4.5.4.0/emby-server-deb_4.5.4.0_amd64.deb
 sudo dpkg -i emby-server-*.deb
+rm emby-server-deb_4.5.4.0_amd64.deb
 rm deploy_emby.sh
