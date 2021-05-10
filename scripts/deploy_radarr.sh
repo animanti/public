76e00578ee23ff766f0a3faee43ec70e758ffa04
animanti <github@mccaull.com.au>
2021-05-10 23:02:37 +1000

    1

--- scripts/deploy_radarr.sh
+++ scripts/deploy_radarr.sh
@@ -7,14 +7,15 @@ sudo apt-get update; \
   sudo apt-get install -y apt-transport-https && \
   sudo apt-get update && \
   sudo apt-get install -y aspnetcore-runtime-5.0
+rm packages-microsoft-prod.deb
 
 #Create User
 sudo adduser --system --group radarr
-sudo gpasswd -a svcmedia radarr
+sudo gpasswd -a radarr radarr
 
 #Download Service Config
 wget https://raw.githubusercontent.com/animanti/public/master/configs/radarr.service
-mv /radarr.service /etc/systemd/system/radarr.service
+mv radarr.service /etc/systemd/system/radarr.service
 
 #Install Radarr https://wiki.servarr.com/Radarr_System#update_to_net_core_version
 wget --content-disposition 'http://radarr.servarr.com/v1/update/master/updatefile?os=linux&runtime=netcore&arch=x64'
@@ -22,11 +23,12 @@ sudo systemctl stop radarr
 sudo mv /opt/Radarr /opt/Radarr.old
 tar -xvzf Radarr*.linux-core-x64.tar.gz
 sudo mv Radarr/ /opt
-sudo chown -R svcmedia:radarr /opt/Radarr
+sudo chown -R radarr:radarr /opt/Radarr
 sudo rm -rf /opt/Radarr.old
 sudo rm -rf Radarr*.linux-core-x64.tar.gz
-sudo nano -e /etc/systemd/system/radarr.service
-	/opt/Radarr/Radarr
+#sudo nano -e /etc/systemd/system/radarr.service
+#	/opt/Radarr/Radarr
 systemctl daemon-reload
-sudo systemctl start radarr.service
+systemctl enable radarr.service
+#sudo systemctl start radarr.service
 rm ./deploy_radarr.sh
