021b6c0e261971ad612817e00ebedaba13fb2a5c
animanti <github@mccaull.com.au>
2021-05-10 22:46:32 +1000

    1

--- scripts/deploy_ombi.sh
+++ scripts/deploy_ombi.sh
@@ -1,4 +1,5 @@
 echo "deb https://apt.ombi.app/develop jessie main" | sudo tee /etc/apt/sources.list.d/ombi.list
 curl -sSL https://apt.ombi.app/pub.key | sudo apt-key add -
 sudo apt update && sudo apt install ombi
-sudo nano /etc/systemd/system/ombi.service
+#sudo nano /etc/systemd/system/ombi.service
+rm ./deploy_ombi.sh
---
+++ scripts/deploy_radarr.sh
@@ -0,0 +1,28 @@
+#RADARR
+#Install Dotnet https://docs.microsoft.com/en-us/dotnet/core/install/linux-ubuntu#2004-
+wget https://packages.microsoft.com/config/ubuntu/20.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
+sudo dpkg -i packages-microsoft-prod.deb
+
+sudo apt-get update; \
+  sudo apt-get install -y apt-transport-https && \
+  sudo apt-get update && \
+  sudo apt-get install -y aspnetcore-runtime-5.0
+
+#Create User
+sudo adduser --system --group radarr
+sudo gpasswd -a svcmedia radarr
+
+#Install Radarr https://wiki.servarr.com/Radarr_System#update_to_net_core_version
+wget --content-disposition 'http://radarr.servarr.com/v1/update/master/updatefile?os=linux&runtime=netcore&arch=x64'
+sudo systemctl stop radarr
+sudo mv /opt/Radarr /opt/Radarr.old
+tar -xvzf Radarr*.linux-core-x64.tar.gz
+sudo mv Radarr/ /opt
+sudo chown -R svcmedia:radarr /opt/Radarr
+sudo rm -rf /opt/Radarr.old
+sudo rm -rf Radarr*.linux-core-x64.tar.gz
+sudo nano -e /etc/systemd/system/radarr.service
+	/opt/Radarr/Radarr
+systemctl daemon-reload
+sudo systemctl start radarr.service
+rm ./deploy_radarr.sh
