121b4aaf3f1056511dfcb069ca32bc208e1a12f9
animanti <github@mccaull.com.au>
2021-05-10 21:59:15 +1000

    scripts

---
+++ scripts/deploy_nginx.sh
@@ -0,0 +1,6 @@
+sudo apt-get update
+sudo apt-get install nginx
+sudo unlink /etc/nginx/sites-enabled/default
+cd etc/nginx/sites-available/
+sudo ln -s /etc/nginx/sites-available/reverse-proxy.conf /etc/nginx/sites-enabled/reverse-proxy.conf
+wget https://raw.githubusercontent.com/animanti/public/master/configs/reverse-proxy.conf
