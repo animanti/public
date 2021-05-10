02ac7278f0943c03bdfe3a8d32c2b1b2e94c4bc4
animanti <github@mccaull.com.au>
2021-05-10 22:15:05 +1000

    1

--- scripts/deploy_nginx.sh
+++ scripts/deploy_nginx.sh
@@ -5,3 +5,7 @@ cd etc/nginx/sites-available/
 sudo ln -s /etc/nginx/sites-available/reverse-proxy.conf /etc/nginx/sites-enabled/reverse-proxy.conf
 wget https://raw.githubusercontent.com/animanti/public/master/configs/reverse-proxy.conf
 mv reverse-proxy.conf /etc/nginx/sites-available/reverse-proxy.conf
+wget https://raw.githubusercontent.com/animanti/public/master/configs/proxy_radarr.conf
+mv ./proxy_radarr.conf /etc/nginx/proxy_radarr.conf
+wget https://raw.githubusercontent.com/animanti/public/master/configs/proxy_qbt.conf
+mv ./proxy_qbt.conf /etc/nginx/proxy_qbt.conf
