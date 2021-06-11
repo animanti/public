34a67b9c33423b7dd4702616f8d192d743fe1b2a
animanti <83048556+animanti@users.noreply.github.com>
2021-06-11 14:31:49 +1000

    Update uBitwarden.ps1

--- apps/uBitwarden.ps1
+++ apps/uBitwarden.ps1
@@ -1,4 +1,4 @@
 ##cd $Env:Programfiles\Bitwarden
 ##.\"Uninstall Bitwarden.exe" /S
-& "$Env:Programfiles\\Bitwarden\\Uninstall Bitwarden.exe" /S
+& "$Env:Programfiles\Bitwarden\Uninstall Bitwarden.exe" /S
 scoop uninstall uBitwarden
